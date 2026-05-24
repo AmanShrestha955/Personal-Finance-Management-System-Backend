ea84fa4fc1b3916ac06e557e9a4ec308ebcfe365
aman <np03cs4a230040@heraldcollege.edu.np>
2026-05-24 23:31:07 +0545

    fix: typo in send Verifiction email

--- src/middlewares/sendVerificationEmail.js
+++ src/middlewares/sendVerificationEmail.js
@@ -1,7 +1,7 @@
 const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendVerificationEmail = async (email, verifiyUrl) => {
+const sendVerificationEmail = async (email, verifyUrl) => {
   await resend.emails.send({
     from: '"Smart Finance" <onboarding@resend.dev>',
     to: email,
